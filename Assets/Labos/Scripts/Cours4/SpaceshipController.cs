cde598e6e6bf7563fbc164f599cf10406fa7465d
Le A <112096529+achamberland11@users.noreply.github.com>
2025-09-25 23:41:02 -0400

    SpaceSHIP!!!!!

--- Assets/Labos/Scripts/Cours4/SpaceshipController.cs
+++ Assets/Labos/Scripts/Cours4/SpaceshipController.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SpaceshipController : SpaceshipControllerBase
 {
-    [SerializeField] private float force = 25f;
-    float gravity = 9.81f;
+    [SerializeField] private const float BASEFORCE = 5f;
+    [SerializeField] private const float DAMPING_FACTOR = 0.1f;
+    private float forceX;
+    private float forceY;
+    float _gravity = 9.81f;
+
     private void Start()
     {
         foreach (RocketEngine engine in _RocketEngines)
@@ -18,23 +20,39 @@ public class SpaceshipController : SpaceshipControllerBase
     private void FixedUpdate()
     {
         Vector3 shipToTargetVector = _TargetPosition - transform.position;
+
+        // Apply force relative to distance
+        forceY = BASEFORCE * Mathf.Abs(shipToTargetVector.y) *0.25f;
+        forceX = BASEFORCE * Mathf.Abs(shipToTargetVector.x) *0.25f;
+
+        Vector3 direction = shipToTargetVector.normalized;
+    
+        // Apply force with normalized distance
+        //forceY = BASEFORCE * Mathf.Abs(direction.y);
+        //forceX = BASEFORCE * Mathf.Abs(direction.x);
         
+        ApplyThrust(shipToTargetVector);
+    }
+
+
+    void ApplyThrust(Vector3 shipToTargetVector)
+    {
         if (shipToTargetVector.y > 0f)
         {
-            _RocketEngines[0].Thrust(force + gravity);
+            _RocketEngines[0].Thrust(forceY + _gravity * (1 - DAMPING_FACTOR));
         }
         else
         {
-            _RocketEngines[3].Thrust(force);
+            _RocketEngines[3].Thrust(forceY - _gravity * (1 - DAMPING_FACTOR));
         }
-        
+
         if (shipToTargetVector.x < 0f)
         {
-            _RocketEngines[1].Thrust(force);
+            _RocketEngines[1].Thrust(forceX * (1 - DAMPING_FACTOR));
         }
         else
         {
-            _RocketEngines[2].Thrust(force);
+            _RocketEngines[2].Thrust(forceX * (1 - DAMPING_FACTOR));
         }
     }
     
